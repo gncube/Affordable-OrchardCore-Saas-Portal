d88ab435a97f8ffff8f66c06bf4a70793e558b4a
Gerald Ncube <gerald.ncube@se.com>
2023-05-09 09:24:13 +0100

    Removed the LocalNugetPackages

---
+++ src/Themes/DigitalAgencyTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Digital Agency Theme",
+    Author = "GSN",
+    Website = "https://ncube.digital",
+    Version = "0.0.1",
+    Description = "Digital Agency Theme"
+)]
---
+++ src/Themes/DigitalAgencyTheme/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+
+namespace DigitalAgencyTheme
+{
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+    {
+        private static ResourceManifest _manifest;
+
+        public ResourceManagementOptionsConfiguration()
+        {
+            _manifest = new ResourceManifest();
+
+            _manifest
+                .DefineStyle("DigitalAgencyTheme-css")
+                .SetUrl("~/DigitalAgencyTheme/assets/css/theme.min.css")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineScript("DigitalAgencyTheme-js")
+                .SetUrl("~/DigitalAgencyTheme/assets/js/theme.min.js")
+                .SetVersion("1.0.0");
+        }
+        public void Configure(ResourceManagementOptions options)
+        {
+            options.ResourceManifests.Add(_manifest);
+        }
+    }
+}
