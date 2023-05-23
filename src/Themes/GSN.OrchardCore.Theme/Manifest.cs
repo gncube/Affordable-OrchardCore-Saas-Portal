85332d1bd9e74957234c41cf08d66d77938d3dc2
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2023-05-23 21:29:00 +0100

    Added GSN.OrchardCore.Theme

---
+++ src/Themes/GSN.OrchardCore.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Thomas Laker Theme",
+    Author = "Thomas Laker Ltd.",
+    Website = "https://geraldncube.co.uk",
+    Version = "1.6.1",
+    Description = "Simple theme created using the EtchUK Theme Boilerplate"
+)]
